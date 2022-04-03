fb109ad8bf6f3bffcb9fb4e659a37161a5aa264c
GabeHasWon <gabriel@guevarameyer.com>
2022-04-03 11:56:28 -0500

    moved the every wal ,

--- Walls/VerdantLeafWall.cs
+++ Walls/VerdantLeafWall.cs
@@ -4,16 +4,16 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Verdant.Items.Verdant.Blocks.Walls;
 
-namespace Verdant.Walls.Verdant
+namespace Verdant.Walls
 {
-    public class LushWoodWall : ModWall
+    public class VerdantLeafWall : ModWall
     {
         public override void SetDefaults()
         {
             Main.wallHouse[Type] = true;
-            dustType = DustID.t_BorealWood;
-            drop = ModContent.ItemType<LushWoodWallItem>();
-            AddMapEntry(new Color(56, 42, 27));
+            dustType = DustID.Grass;
+            drop = ModContent.ItemType<VerdantLeafWallItem>();
+            AddMapEntry(new Color(20, 82, 39));
         }
 
         public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
--- Walls/VerdantVineWall_Unsafe.cs
+++ Walls/VerdantVineWall_Unsafe.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Verdant.Walls.Verdant
+namespace Verdant.Walls
 {
     public class VerdantVineWall_Unsafe : ModWall
     {
