cc770e227ab6963d736bb0a50c25ac6972acc707
GabeHasWon <gabriel@guevarameyer.com>
2022-04-12 19:40:49 -0500

    lush flower polish

--- Projectiles/Minion/VerdantHealingMinion.cs
+++ Projectiles/Minion/VerdantHealingMinion.cs
@@ -13,10 +13,9 @@ namespace Verdant.Projectiles.Minion
     class VerdantHealingMinion : ModProjectile, IDrawAdditive
     {
         ref float State => ref projectile.ai[0];
+        ref float Timer => ref projectile.ai[1];
 
         public Vector2 goPosition = -Vector2.One;
-
-        private int timer = 0;
 
         public override void SetStaticDefaults()
         {
@@ -43,9 +42,9 @@ namespace Verdant.Projectiles.Minion
 
         public override void AI()
         {
-            timer++;
+            Timer++;
 
-            float sc = 0.5f - (float)(Math.Sin(timer * 0.03f) * 0.1f);
+            float sc = 0.5f - (float)(Math.Sin(Timer * 0.03f) * 0.1f);
 
             if (projectile.velocity.Length() < 3f)
                 Lighting.AddLight(projectile.Center, new Vector3(0.4f, 0.12f, 0.24f) * 6 * ((3f - projectile.velocity.Length()) / 3) * sc);
@@ -53,18 +52,24 @@ namespace Verdant.Projectiles.Minion
             for (int i = 0; i < Main.player.Length; ++i)
             {
                 Player p = Main.player[i];
-                Vector2 off = new Vector2(0, (float)(Math.Sin(timer * 0.03f) * 6));
-                float rad = .9f - ((float)(Math.Sin(timer * 0.03f) * 0.05f));
+                Vector2 off = new Vector2(0, (float)(Math.Sin(Timer * 0.03f) * 6));
+                float rad = .9f - ((float)(Math.Sin(Timer * 0.03f) * 0.05f));
 
                 float radMult = 1f;
                 if (State == 1) radMult = 1.2f;
 
-                if (timer % 210 == 0 && p.active && !p.dead && Vector2.Distance(p.MountedCenter, projectile.Center - off) < rad * radMult * 196)
+                if (Timer % 210 == 120 && p.active && !p.dead && Vector2.Distance(p.MountedCenter, projectile.Center - off) < rad * radMult * 196)
                 {
                     if (p.statLife < p.statLifeMax2 - 10)
                     {
                         p.HealEffect(10, true);
                         p.statLife += 10;
+
+                        for (int j = 0; j < 3; ++j)
+                        {
+                            Vector2 particleVel = new Vector2(Main.rand.NextFloat(4, 12), 0).RotatedByRandom(MathHelper.TwoPi);
+                            Projectile.NewProjectile(projectile.Center, particleVel, ModContent.ProjectileType<Misc.HealingParticle>(), 0, 0, projectile.owner, 0, 0);
+                        }
                     }
                 }
             }
@@ -115,17 +120,19 @@ namespace Verdant.Projectiles.Minion
         private void DrawCircle(SpriteBatch spriteBatch, Color lightColor, float scale, out Vector2 off)
         {
             Texture2D circle = ModContent.GetTexture("Verdant/Projectiles/Minion/VerdantHealingCircle"); Sandstorm.TimeLeft = 1800;
-            float sc = 1.9f - (float)(Math.Sin(timer * 0.03f) * 0.05f);
+            float sc = 1.9f - (float)(Math.Sin(Timer * 0.03f) * 0.05f);
             float alphaScale = (sc - 1.78f) * 8;
             Color circleCol = projectile.GetAlpha(lightColor) * alphaScale;
 
-            off = new Vector2(0, (float)(Math.Sin(timer * 0.03f) * 6));
+            off = new Vector2(0, (float)(Math.Sin(Timer * 0.03f) * 6));
 
-            spriteBatch.Draw(circle, projectile.Center - Main.screenPosition - off, circle.Frame(), circleCol, timer * 0.006f, circle.Bounds.Center.ToVector2(), sc * scale, SpriteEffects.None, 1f);
+            spriteBatch.Draw(circle, projectile.Center - Main.screenPosition - off, circle.Frame(), circleCol, Timer * 0.006f, circle.Bounds.Center.ToVector2(), sc * scale, SpriteEffects.None, 1f);
             if (alphaScale > 0.95f)
             {
-                spriteBatch.Draw(circle, projectile.Center - Main.screenPosition - off, circle.Frame(), projectile.GetAlpha(lightColor) * ((alphaScale - 0.95f) * 1.5f), timer * 0.006f, circle.Bounds.Center.ToVector2(), sc * scale * 1.04f, SpriteEffects.None, 1f);
-                spriteBatch.Draw(circle, projectile.Center - Main.screenPosition - off, circle.Frame(), projectile.GetAlpha(lightColor) * ((alphaScale - 0.95f) * 1.5f), timer * 0.006f, circle.Bounds.Center.ToVector2(), sc * scale * 0.96f, SpriteEffects.None, 1f);
+                Color col = projectile.GetAlpha(lightColor) * ((alphaScale - 0.95f) * 1.5f);
+
+                spriteBatch.Draw(circle, projectile.Center - Main.screenPosition - off, circle.Frame(), col, Timer * 0.006f, circle.Bounds.Center.ToVector2(), sc * scale * 1.04f, SpriteEffects.None, 1f);
+                spriteBatch.Draw(circle, projectile.Center - Main.screenPosition - off, circle.Frame(), col, Timer * 0.006f, circle.Bounds.Center.ToVector2(), sc * scale * 0.96f, SpriteEffects.None, 1f);
             }
         }
 
@@ -136,7 +143,7 @@ namespace Verdant.Projectiles.Minion
                 Texture2D tex = mod.GetTexture("Textures/Circle");
                 float rot = projectile.velocity.ToRotation();
                 Vector2 scale = new Vector2(1 + (projectile.velocity.Length() * 0.02f), 1) * 5f;
-                float sc = 0.9f - (float)(Math.Sin(timer * 0.03f) * 0.05f);
+                float sc = 0.9f - (float)(Math.Sin(Timer * 0.03f) * 0.05f);
 
                 Main.spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, new Color(0, 170, 62) * 0.45f, rot, tex.Size() / 2f, scale * sc, SpriteEffects.None, 1f);
             }
