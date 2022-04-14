f47d0c5f6fd423f9e94243989b91cec4bcb9b2f0
GabeHasWon <gabriel@guevarameyer.com>
2022-04-13 20:57:26 -0500

    soil wall item + walle

---
+++ Items/Verdant/Blocks/Walls/LushSoilWallItem.cs
@@ -0,0 +1,16 @@
+﻿using Terraria.ModLoader;
+using Verdant.Walls;
+
+namespace Verdant.Items.Verdant.Blocks.Walls
+{
+    public class LushSoilWallItem : ModItem
+    {
+        public override void SetStaticDefaults() => QuickItem.SetStatic(this, "Lush Soil Wall", "");
+        public override void SetDefaults() => QuickItem.SetWall(this, 32, 32, ModContent.WallType<LushSoilWall>());
+        public override void AddRecipes()
+        {
+            QuickItem.AddRecipe(this, mod, -1, 4, (ModContent.ItemType<LushSoilBlock>(), 1));
+            QuickItem.AddRecipe(ModContent.ItemType<LushSoilBlock>(), mod, -1, 1, (item.type, 4));
+        }
+    }
+}
--- Walls/LushSoilWall_Unsafe.cs
+++ Walls/LushSoilWall_Unsafe.cs
@@ -17,4 +17,23 @@ namespace Verdant.Walls
 
         public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
     }
+
+    public class LushSoilWall : ModWall
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = "Verdant/Walls/LushSoilWall_Unsafe";
+            return base.Autoload(ref name, ref texture);
+        }
+
+        public override void SetDefaults()
+        {
+            Main.wallHouse[Type] = false;
+            dustType = DustID.Grass;
+            drop = 0;
+            AddMapEntry(new Color(50, 15, 15));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
+    }
 }
