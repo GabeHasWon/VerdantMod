3dcf0756b37cebf4b1e348d98b526fcc8b4b97e0
GabeHasWon <gabriel@guevarameyer.com>
2023-01-04 21:42:49 -0600

    vibrant horizons box

---
+++ Tiles/Verdant/Decor/MusicBox/BaseMusicBox.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Verdant.Tiles.Verdant.Decor.MusicBox;
+
+public abstract class BaseMusicBox<T> : ModTile where T : ModItem
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileFrameImportant[Type] = true;
+		Main.tileObsidianKill[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+		TileObjectData.newTile.Origin = new Point16(0, 1);
+		TileObjectData.newTile.LavaDeath = false;
+		TileObjectData.newTile.DrawYOffset = 2;
+		TileObjectData.addTile(Type);
+
+		ModTranslation name = CreateMapEntryName();
+		name.SetDefault("Music Box");
+		AddMapEntry(new Color(200, 200, 200), name);
+	}
+
+	public override void KillMultiTile(int i, int j, int frameX, int frameY) => Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 48, ModContent.ItemType<T>());
+
+	public override void MouseOver(int i, int j)
+	{
+		Player player = Main.LocalPlayer;
+		player.noThrow = 2;
+		player.cursorItemIconEnabled = true;
+		player.cursorItemIconID = ModContent.ItemType<T>();
+	}
+}
