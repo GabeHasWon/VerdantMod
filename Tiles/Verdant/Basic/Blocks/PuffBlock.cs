3ee4fc01f25d58c77a46ddeb4304c0b7bda06120
GabeHasWon <gabriel@guevarameyer.com>
2023-07-27 11:51:32 -0500

    builds on 1.4.4, not in vs tho

--- Tiles/Verdant/Basic/Blocks/PuffBlock.cs
+++ Tiles/Verdant/Basic/Blocks/PuffBlock.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Verdant.Items.Verdant.Blocks;
-using Verdant.Items.Verdant.Blocks.Puff;
 
 namespace Verdant.Tiles.Verdant.Basic.Blocks;
 
