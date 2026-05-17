5f9885201d2c8bb9682398dc0c4766d1c87b3864
GabeHasWon <gabehw.contact@gmail.com>
2026-05-17 17:28:31 -0500

    Made Aquamarine count as a Gemstone item

---
+++ Items/Global/VerdantItemSets.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ID;
+
+namespace Verdant.Items.Global;
+
+internal class VerdantItemSets
+{
+    public static readonly bool[] Gemstone = ItemID.Sets.Factory.CreateNamedSet(nameof(Gemstone)).Description("Whether this item is a gem").RegisterBoolSet(ItemID.Amethyst, ItemID.Topaz, 
+        ItemID.Sapphire, ItemID.Ruby, ItemID.Emerald, ItemID.Diamond, ItemID.Amber);
+}
--- Items/Verdant/Blocks/Aquamarine/AquamarineItem.cs
+++ Items/Verdant/Blocks/Aquamarine/AquamarineItem.cs
@@ -1,9 +1,11 @@
 ﻿using Terraria.ModLoader;
+using Verdant.Items.Global;
 using Verdant.Tiles.Verdant.Basic.Aquamarine;
 
 namespace Verdant.Items.Verdant.Blocks.Aquamarine;
 
 public class AquamarineItem : ModItem
 {
+    public override void SetStaticDefaults() => VerdantItemSets.Gemstone[Type] = true;
     public override void SetDefaults() => QuickItem.SetBlock(this, 28, 28, ModContent.TileType<AquamarineTile>());
 }
